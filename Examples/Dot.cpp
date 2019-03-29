cc48980715eb3882e08106723bbd186a87c11a6e
Kern Handa <kern.handa@gmail.com>
2019-03-29 00:08:17 -0700

    Multi-QPU Dot

--- Examples/Dot.cpp
+++ Examples/Dot.cpp
@@ -12,18 +12,19 @@ using namespace QPULib;
 
 void dot(Int N, Ptr<Float> A, Ptr<Float> B, Ptr<Float> result)
 {
-    Int inc = numQPUs() << 4;
+    Int inc = 16;//numQPUs() << 4;
     Int qpuID = me();
     Ptr<Float> a = A + index() + (qpuID << 4);
     Ptr<Float> b = B + index() + (qpuID << 4);
+    Ptr<Float> c = result + index() + (qpuID << 4);
     gather(a); gather(b);
 
     Float aOld, bOld;//, temp = 0;
     For (Int i = 0, i < N, i = i + inc)
-        gather(a + i + inc);  gather(b + i + inc);
+        gather(a + i + inc+ (qpuID << 4));  gather(b + i + inc+ (qpuID << 4));
         receive(aOld); receive(bOld);
         // temp = temp + (aOld * bOld);
-        store(aOld * bOld, result + i + (qpuID << 4));
+        store(aOld * bOld, c + i+ (qpuID << 4));
     End
 
     // store(temp, result + (qpuID << 4));
@@ -38,13 +39,13 @@ int main()
   // Timestamps
   timeval tvStart, tvEnd, tvDiff;
 
-  const int N = 16 * 100; // 192000
+  const int N = 16 * 24; // 192000
 
   // Construct kernel
   auto k = compile(dot);
 
   // Use 12 QPUs
-  k.setNumQPUs(1);
+  k.setNumQPUs(12);
 
   // Allocate and initialise arrays shared between ARM and GPU
   SharedArray<float> x(N), y(N), result(N);
@@ -70,14 +71,14 @@ int main()
   printf("BLAS: %ld.%06lds\n", tvDiff.tv_sec, tvDiff.tv_usec);
   printf("blasOutput = %f\n", blasOut);
 
-  // printf("\nIndex,A,B,GPU,CPU,Delta\n");
-  // for (int i = 0; i < N; ++i) {
-  //   float a = x[i], b = y[i];
-  //   float gpu = result[i];
-  //   float cpu = a * b;
-  //   float delta = cpu - gpu;
-  //   printf("%d,%f,%f,%f,%f,%f\n", i, a, b, gpu, cpu, delta);
-  // }
+  printf("\nIndex,A,B,GPU,CPU,Delta\n");
+  for (int i = 0; i < N; ++i) {
+    float a = x[i], b = y[i];
+    float gpu = result[i];
+    float cpu = a * b;
+    float delta = cpu - gpu;
+    printf("%d,%f,%f,%f,%f,%f\n", i, a, b, gpu, cpu, delta);
+  }
 
   return 0;
 }
