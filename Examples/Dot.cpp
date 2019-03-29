91ae321233650a7195410cd8f410397d712b09d1
Kern Handa <kern.handa@gmail.com>
2019-03-29 01:48:53 -0700

    WIP - doesn't work

--- Examples/Dot.cpp
+++ Examples/Dot.cpp
@@ -19,15 +19,16 @@ void dot(Int N, Ptr<Float> A, Ptr<Float> B, Ptr<Float> result)
     Ptr<Float> c = result + index() + (qpuID << 4);
     gather(a); gather(b);
 
-    Float aOld, bOld;//, temp = 0;
+    Float aOld, bOld;
+    Float temp = result[qpuID << 4];
     For (Int i = 0, i < N, i = i + inc)
         gather(a + i + inc+ (qpuID << 4));  gather(b + i + inc+ (qpuID << 4));
         receive(aOld); receive(bOld);
-        // temp = temp + (aOld * bOld);
-        store(aOld * bOld, c + i+ (qpuID << 4));
+        temp = temp + (aOld * bOld);
+        // store(aOld * bOld, c + i+ (qpuID << 4));
     End
 
-    // store(temp, result + (qpuID << 4));
+    store(temp, c);
 }
 
 // ============================================================================
@@ -48,8 +49,8 @@ int main()
   k.setNumQPUs(12);
 
   // Allocate and initialise arrays shared between ARM and GPU
-  SharedArray<float> x(N), y(N), result(N);
-  std::fill_n(&result[0], N, 0.0f);
+  SharedArray<float> x(N), y(N), result(16 * k.numQPUs);
+  std::fill_n(&result[0], result.size, 0.0f);
   std::random_device rd;
   std::default_random_engine engine(rd());
   std::uniform_real_distribution<float> dist;
@@ -58,7 +59,7 @@ int main()
 
   gettimeofday(&tvStart, NULL);
   k(N, &x, &y, &result);
-  float gpuOut = std::accumulate(&result[0], &result[0] + N, 0.f);
+  float gpuOut = std::accumulate(&result[0], &result[0] + result.size, 0.f);
   gettimeofday(&tvEnd, NULL);
   timersub(&tvEnd, &tvStart, &tvDiff);
   printf("GPU: %ld.%06lds\n", tvDiff.tv_sec, tvDiff.tv_usec);
@@ -71,14 +72,14 @@ int main()
   printf("BLAS: %ld.%06lds\n", tvDiff.tv_sec, tvDiff.tv_usec);
   printf("blasOutput = %f\n", blasOut);
 
-  printf("\nIndex,A,B,GPU,CPU,Delta\n");
-  for (int i = 0; i < N; ++i) {
-    float a = x[i], b = y[i];
-    float gpu = result[i];
-    float cpu = a * b;
-    float delta = cpu - gpu;
-    printf("%d,%f,%f,%f,%f,%f\n", i, a, b, gpu, cpu, delta);
-  }
+  // printf("\nIndex,A,B,GPU,CPU,Delta\n");
+  // for (int i = 0; i < N; ++i) {
+  //   float a = x[i], b = y[i];
+  //   float gpu = result[i];
+  //   float cpu = a * b;
+  //   float delta = cpu - gpu;
+  //   printf("%d,%f,%f,%f,%f,%f\n", i, a, b, gpu, cpu, delta);
+  // }
 
   return 0;
 }
